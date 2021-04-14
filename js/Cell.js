b0e94d799188b5b0d956016a85dae38de693e556
akasmirhan4 <amirrul.kasmirhan@gmail.com>
2021-04-14 10:50:53 +0800

    fixed bug when resizing made game crash

--- js/Cell.js
+++ js/Cell.js
@@ -13,23 +13,23 @@ class Cell {
             if (this.parent[this.x][this.y - 1].isAlive) {
                 nAlive++;
             }
-            if (this.x < this.parent[0].length-1) {
+            if (this.x < this.parent.length-1) {
                 if (this.parent[this.x + 1][this.y - 1].isAlive) {
                     nAlive++;
                 }
             }
         }
-        if (this.x < this.parent[0].length-1) {
+        if (this.x < this.parent.length-1) {
             if (this.parent[this.x + 1][this.y].isAlive) {
                 nAlive++;
             }
-            if (this.y < this.parent.length-1) {
+            if (this.y < this.parent[0].length-1) {
                 if (this.parent[this.x + 1][this.y + 1].isAlive) {
                     nAlive++;
                 }
             }
         }
-        if (this.y < this.parent.length-1) {
+        if (this.y < this.parent[0].length-1) {
             if (this.parent[this.x][this.y + 1].isAlive) {
                 nAlive++;
             }
